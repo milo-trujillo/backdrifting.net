a10ce8e29786bfdc550ac5c5ddd7e85d4dbd6177
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2022-12-23 18:49:22 -0700

    Strip newer authentication fields and metadata

--- cleanup.sh
+++ cleanup.sh
@@ -5,4 +5,13 @@
 DIR=$(dirname "$SCRIPT")
 
 sed -i -e 's/^PreviewPassword.*$/PreviewPassword = ""/' $DIR/backdrifting.rb
+sed -i -e 's/^ShareablePreviewPassword.*$/ShareablePreviewPassword = ""/' $DIR/backdrifting.rb
+sed -i -e 's/^AnalyticsPassword.*$/AnalyticsPassword = ""/' $DIR/backdrifting.rb
+sed -i -e 's/^SiteName.*$/SiteName = ""/' $DIR/backdrifting.rb
+sed -i -e 's/^SiteURL.*$/SiteURL = ""/' $DIR/backdrifting.rb
+sed -i -e 's/^SiteDomains.*$/SiteDomains = ["example.com", "www.example.com"]/' $DIR/backdrifting.rb
+sed -i -e 's/^TwitterHandle.*$/TwitterHandle = "@foo"/' $DIR/backdrifting.rb
+sed -i -e 's/^Author.*$/Author = ""/' $DIR/backdrifting.rb
+sed -i -e 's/^Description.*$/Description = "Digital Haven"/' $DIR/backdrifting.rb
+sed -i -e 's/^AnalyticsEnabled.*$/AnalyticsEnabled = false/' $DIR/backdrifting.rb
 find "${DIR}" -name "*-e" -exec rm {} \;
